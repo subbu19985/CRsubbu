13cb7a217ec9d0531cffcde1a48d808c58a17f58
subbu19985 <52446307+subbu19985@users.noreply.github.com>
2019-07-03 09:43:43 +0530

    Create prog5.py

---
+++ prog5.py
@@ -0,0 +1,7 @@
+ch = input("Enter a character :")
+if(ch== '$' or ch== '#' or ch=='%' or ch=='&'):
+    print("invalid")
+elif(ch=='a' or ch=='e' or ch=='i' or ch=='o' or ch=='u'):
+    print("Vowel")
+else:
+    print("Consonant")
