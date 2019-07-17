9f081c42f0589ba839ea957d28832abdef9e6a99
subbu19985 <52446307+subbu19985@users.noreply.github.com>
2019-07-17 10:07:49 +0530

    Create 777.py

---
+++ 777.py
@@ -0,0 +1,15 @@
+sh=int(input())
+pt=[]
+for i in range(sh):
+  ass=input()
+  pt.append(ass)
+ma1=min(pt,key=len)
+pt.remove(ma1)
+for i in range(len(ma1)):
+  for j in range(len(pt)):
+     ba1=pt[j]
+     if ma1[:i+1]==ba1[:i+1]:
+       result=ma1[:i+1]
+     else:
+       break
+print(result)
