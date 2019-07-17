8de6b872202bff3aa2bffc6b8ecb2b32b43b001c
subbu19985 <52446307+subbu19985@users.noreply.github.com>
2019-07-17 10:12:09 +0530

    Create 677.py

---
+++ 677.py
@@ -0,0 +1,15 @@
+ch=int(input())
+qt=[]
+for i in range(ch):
+  ass=input()
+  qt.append(ass)
+ma2=min(qt,key=len)
+qt.remove(ma2)
+for i in range(len(ma2)):
+  for j in range(len(qt)):
+     ba1=qt[j]
+     if ma2[:i+1]==ba1[:i+1]:
+       result=ma2[:i+1]
+     else:
+       break
+print(result)
